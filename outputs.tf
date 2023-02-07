f0d1602198b1b269f0d1c179c96717e8e0a34698
Benoit Garcia <benoit.garcia@protonmail.com>
2023-02-07 20:15:33 +0100

    chores(syntax): Fix formatting.

--- outputs.tf
+++ outputs.tf
@@ -10,16 +10,16 @@ output "instance_id" {
 
 output "load_balancer" {
   description = "List of load balancer endpoints of the database instance."
-  value = scaleway_rdb_instance.this.load_balancer
+  value       = scaleway_rdb_instance.this.load_balancer
 }
 
 output "replicate_pn" {
   description = "Private network attributes of the read replica."
-  value = scaleway_rdb_read_replica.this.*.private_network
+  value       = scaleway_rdb_read_replica.this.*.private_network
 }
 
 output "user_password" {
   description = "Password generated if non were given."
-  sensitive = true
-  value = try(random_password.this.0.result, null)
+  sensitive   = true
+  value       = try(random_password.this.0.result, null)
 }
